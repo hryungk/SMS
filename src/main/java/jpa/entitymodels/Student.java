40ed5d2d47cf8988485ab2f1eea31e1c7c596c16
HoppyCat <hryungk@gmail.com>
2021-07-14 22:03:04 -0700

    Requirement 2-4

---
+++ src/main/java/jpa/entitymodels/Student.java
@@ -0,0 +1,68 @@
+package jpa.entitymodels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Student")
+public class Student {
+
+	/* Make sure the table name and the field name match by either
+	 * changing the field name or
+	 * using annotation
+	 * */ 
+	@Id
+	@Column(name="email")
+	private String sEmail; // Student's current school email, unique student identifier
+	@Column(name="name")
+	private String sName; // The full name of the student
+	@Column(name="password")
+	private String sPass; // Student's password in order to login
+//	@OneToMany(targetEntity = Course.class)
+	private List sCourses; // All the courses that a student's registered for
+	
+	public Student() {
+		sEmail = "";
+		sName = "";
+		sPass = "";
+		sCourses = new ArrayList<>();
+	}
+	public Student(String sEmail, String sName, String sPass, List sCourses) {
+		super();
+		this.sEmail = sEmail;
+		this.sName = sName;
+		this.sPass = sPass;
+		this.sCourses = sCourses;
+	}
+	public String getsEmail() {
+		return sEmail;
+	}
+	public void setsEmail(String sEmail) {
+		this.sEmail = sEmail;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	public String getsPass() {
+		return sPass;
+	}
+	public void setsPass(String sPass) {
+		this.sPass = sPass;
+	}
+	public List getsCourses() {
+		return sCourses;
+	}
+	public void setsCourses(List sCourses) {
+		this.sCourses = sCourses;
+	}	
+		
+}
---
+++ src/main/java/jpa/service/CourseService.java
@@ -0,0 +1,16 @@
+package jpa.service;
+
+import java.util.List;
+
+import jpa.dao.CourseDAO;
+import jpa.entitymodels.Course;
+
+public class CourseService implements CourseDAO {
+
+	@Override
+	public List<Course> getAllCourses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
